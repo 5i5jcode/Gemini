13b6b2c50db6c551b0c5606245e739853de51281
coffeefoam <coffeefoam@126.com>
2014-03-15 19:34:45 +0800

    update

---
+++ src/test/java/com/bacic5i5j/test/database/SessionContextTest.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2014, 5i5j.com. All rights reserved.
+ * 5i5j.com. Use is subject to license terms.
+ */
+package com.bacic5i5j.test.database;
+
+/**
+ * @(#)SessionContextTest.java 1.0 15/03/2014
+ */
+public class SessionContextTest {
+}
